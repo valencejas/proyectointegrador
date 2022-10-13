2f99506fc86148ee43885345aed3e5c9c1948949
Jcejas1998 <107890215+valencejas@users.noreply.github.com>
2022-10-13 11:32:19 -0300

    sigue

--- BackEnd/src/main/java/com/cejasportafolio/mgb/seguridad/MainSeguridad.java
+++ BackEnd/src/main/java/com/cejasportafolio/mgb/seguridad/MainSeguridad.java
@@ -5,21 +5,14 @@
 package com.cejasportafolio.mgb.seguridad;
 
 import com.cejasportafolio.mgb.seguridad.jwt.JwtEntryPoint;
-import com.cejasportafolio.mgb.seguridad.jwt.JwtTokenFilter;
+
 import com.cejasportafolio.mgb.seguridad.servicio.Implementacion;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
@@ -30,42 +23,6 @@ public class MainSeguridad extends WebSecurityConfigurerAdapter {
    Implementacion implementacion ;
      @Autowired 
      JwtEntryPoint jwtEntryPoint;
-     @Bean 
-     public JwtTokenFilter jwtTokenFilter(){
-         return new JwtTokenFilter();
-     }
-     @Bean 
-     public PasswordEncoder passwordEncoder(){
-         return new BCryptPasswordEncoder();
-     }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
-
-    @Override
-    protected AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
-    }
-@Bean 
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(implementacion).passwordEncoder(passwordEncoder());
-    }
-
+      
      
 }
