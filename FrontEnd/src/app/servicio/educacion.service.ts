77522d4aa1c22e1fb1f934f55c492789c2a84e5d
Jcejas1998 <107890215+valencejas@users.noreply.github.com>
2022-10-15 12:48:27 -0300

    corrigiendo

--- FrontEnd/src/app/servicio/educacion.service.ts
+++ FrontEnd/src/app/servicio/educacion.service.ts
@@ -7,7 +7,7 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class EducacionService {
-  URL = "http://localhost:8080/educacion/";
+  URL = "http://localhost:8080/educacion";
 
   constructor(private httpClient: HttpClient ) { }
 
